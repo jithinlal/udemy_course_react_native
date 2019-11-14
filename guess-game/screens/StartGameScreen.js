450b826252a491bccfb4b74d3ec3d85b98b1fa49
jithinlal <jithtitan007@gmail.com>
2019-11-14 15:58:02 +0530

    14/11/19 - guess game

---
+++ guess-game/screens/StartGameScreen.js
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+import {
+	Button,
+	View,
+	StyleSheet,
+	Text,
+	TouchableWithoutFeedback,
+	Keyboard,
+	Alert,
+} from 'react-native';
+
+import Card from '../components/Card';
+import Input from '../components/Input';
+import Colors from '../constants/Color';
+import NumberContainer from '../components/NumberContainer';
+
+const StartGameScreen = props => {
+	const [enteredValue, setEnteredValue] = useState('');
+	const [confirmed, setConfirmed] = useState(false);
+	const [selectedNumber, setSelectedNumber] = useState();
+
+	const numberInputHandler = inputText => {
+		setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+	};
+
+	const resetInputHandler = () => {
+		setEnteredValue('');
+		setConfirmed(false);
+	};
+
+	const confirmInputHandler = () => {
+		const chosenNumber = parseInt(enteredValue);
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			setConfirmed(false);
+			Alert.alert('Invalid Number!', 'Number needs to be between 0 and 99', [
+				{
+					text: 'Okay',
+					style: 'destructive',
+					onPress: resetInputHandler,
+				},
+			]);
+			return;
+		}
+		setConfirmed(true);
+		setSelectedNumber(chosenNumber);
+		setEnteredValue('');
+		Keyboard.dismiss();
+	};
+
+	let confirmedOutput;
+	if (confirmed) {
+		confirmedOutput = (
+			<Card style={styles.summaryContainer}>
+				<Text>You Selected</Text>
+				<NumberContainer>{selectedNumber}</NumberContainer>
+				<Button title='START GAME' />
+			</Card>
+		);
+	}
+
+	return (
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}
+		>
+			<View style={styles.screen}>
+				<Text>Start New Game</Text>
+				<Card style={styles.inputContainer}>
+					<Text style={styles.title}>Select a Number</Text>
+					<Input
+						style={styles.input}
+						blurOnSubmit
+						autoCapitalize='characters'
+						autoCorrect={false}
+						keyboardType='number-pad'
+						maxLength={2}
+						onChangeText={numberInputHandler}
+						value={enteredValue}
+					/>
+					<View style={styles.buttonContainer}>
+						<View style={styles.button}>
+							<Button
+								title='Reset'
+								onPress={resetInputHandler}
+								color={Colors.accent}
+							/>
+						</View>
+						<View>
+							<Button
+								title='Confirm'
+								onPress={confirmInputHandler}
+								color={Colors.primary}
+							/>
+						</View>
+					</View>
+				</Card>
+				{confirmedOutput}
+			</View>
+		</TouchableWithoutFeedback>
+	);
+};
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		padding: 10,
+		alignItems: 'center',
+	},
+	title: {
+		fontSize: 20,
+		marginVertical: 10,
+	},
+	inputContainer: {
+		width: 300,
+		maxWidth: '80%',
+		alignItems: 'center',
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		width: '100%',
+		justifyContent: 'space-between',
+		paddingHorizontal: 15,
+	},
+	button: {
+		width: 100,
+	},
+	input: {
+		width: 50,
+		textAlign: 'center',
+	},
+	summaryContainer: {
+		marginTop: 20,
+		alignItems: 'center',
+	},
+});
+
+export default StartGameScreen;
