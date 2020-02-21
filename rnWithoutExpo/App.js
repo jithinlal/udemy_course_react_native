10bd8b70cdadaa5ea8a6fc5192d632fc28278466
jithinlal <jithinlal@softwareassociates.in>
2020-02-21 10:43:04 +0530

    without expo

---
+++ rnWithoutExpo/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {StyleSheet, View, Button} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+const App = () => {
+  const pickImage = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        console.log(response.uri);
+      }
+    });
+  };
+
+  return (
+    <>
+      <View style={styles.screen}>
+        <Button title="Take Image" onPress={pickImage} />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
